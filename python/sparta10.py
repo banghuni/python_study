2cf33b07ab8b72c6d167e3db25c78aa2107c895f
jangbyunghun <qudgns313@naver.com>
2025-07-04 16:15:37 +0900

    빡시노

---
+++ python/sparta10.py
@@ -0,0 +1,10 @@
+a =(input("하나의 문자 입력:"))
+
+if a == 'R' or a == 'r':
+    print("Rectangle")
+elif a =='T' or a == 't':
+    print("Triangle")
+elif a == 'c' or a == 'C':
+    print("Circle")
+else:
+    print("Unknwon")
